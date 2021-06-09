a39ec955025d4acbd6af550bec303f5db9522aa4
Luis <Luis@LAPTOP-PTJO4A3R>
2021-06-09 14:01:08 -0500

    funcion en modeloUser p quitar campos de resp json

--- models/usuario.js
+++ models/usuario.js
@@ -32,4 +32,11 @@ const UsuarioSchema = Schema({
     }
 });
 
+
+UsuarioSchema.methods.toJSON = function() {
+
+    const { __v, password, ...user } = this.toObject();
+    return user;
+}
+
 module.exports = model('Usuario', UsuarioSchema);
